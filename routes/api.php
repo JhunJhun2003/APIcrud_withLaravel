69c715ab55c14c282d8f186cd6a6557c6fbeb991
kokyaw3482@gmail.com <myanmardatascience@gmail.com>
2026-02-10 23:56:54 +0630

    done crud

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::get("/posts", function () {
-    return 'API is working';
-}); 
+Route::apiResource("posts", PostController::class);
+
+
+// Route::get("/posts", function () {
+//     return 'API is working';
+// });  
